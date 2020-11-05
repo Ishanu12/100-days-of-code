b28314dbbd4a71a83443fcda12956656adbc25a5
Ishanu12 <bansalisha056@gmail.com>
2020-11-05 23:38:07 +0530

    day 5

---
+++ day5/Merge Two Sorted Lists.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    {
+        if(l1==NULL && l2==NULL)
+        {
+            return NULL;
+        }
+        if(l1==NULL)
+        {
+            return l2;
+        }
+        if(l2==NULL)
+        {
+            return l1;
+        }
+        ListNode* ft=NULL;
+        ListNode* fh=NULL;
+        if(l1->val<=l2->val)
+        {
+            fh=l1;
+            ft=l1;
+            l1=l1->next;
+        }
+        else
+        {
+            fh=l2;
+            ft=l2;
+            l2=l2->next;
+        }
+        while(l1!=NULL && l2!=NULL)
+        {
+            if(l1->val<=l2->val)
+            {
+                ft->next=l1;
+                l1=l1->next;
+            }
+            else
+            {
+                ft->next=l2;
+                l2=l2->next;
+            }
+            ft=ft->next;
+        }
+        if(l1!=NULL)
+        {
+            ft->next=l1;
+        }
+        if(l2!=NULL)
+        {
+            ft->next=l2;
+        }
+        return fh;
+    }
+};
